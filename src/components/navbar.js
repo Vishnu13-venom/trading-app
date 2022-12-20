785be7fdd466a96bd87def49bd999bae49fdd832
Vishnu <vishnu9716670495@gmail.com>
2022-12-20 14:36:41 +0530

    first commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -19,45 +15,7 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -168,6 +126,7 @@ export default function PrimarySearchAppBar() {
               fontSize: '1.2rem',
               justifyContent: 'space-between',
               textDecoration: 'none',
+              // eslint-disable-next-line
               display: 'block',
               mx:'-1.2rem',
               }}
@@ -182,6 +141,7 @@ export default function PrimarySearchAppBar() {
               font:'Overpass',
               fontSize: '0.8rem',
               textDecoration: 'none',
+              // eslint-disable-next-line
               display: 'block',
               mx:'1rem',
               ml:'4rem'
@@ -199,6 +159,7 @@ export default function PrimarySearchAppBar() {
               font:'Overpass',
               fontSize: '0.8rem',
               textDecoration: 'none',
+              // eslint-disable-next-line
               display: 'block',
               mx:'1rem',
               }}
@@ -214,6 +175,7 @@ export default function PrimarySearchAppBar() {
               font:'Overpass',
               fontSize: '0.8rem',
               textDecoration: 'none',
+              // eslint-disable-next-line
               display: 'block',
               mx:'1rem',
               }}
@@ -229,6 +191,7 @@ export default function PrimarySearchAppBar() {
               font:'Overpass',
               fontSize: '0.8rem',
               textDecoration: 'none',
+              // eslint-disable-next-line
               display: 'block',
               mx:'1rem',
               }}
